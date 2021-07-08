b482556c026f2b41fe6b200efa8a09fef06abc4f
kanno1123 <ks011123@outlook.jp>
2021-07-08 21:10:57 +0900

    Initial revision

---
+++ Diceroll.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int i1 = 0;
+  int i2 = 0;
+  
+  printf("Rolling the dice...\n");
+  i1 = rand()%6 + 1;
+  printf("Die 1 : %d\n",i1);
+  i2 = rand()%6 + 1;
+  printf("Die 2 : %d\n",i2);
+  printf("Total value: %d\n",i1+i2);
+ 
+  return 0;
+}
